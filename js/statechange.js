274b0dda45f11671987781249d741bc23ca7c408
saverill24 <saverill24@gmail.com>
2023-02-28 12:47:09 -0500

    javascript colors

---
+++ js/statechange.js
@@ -0,0 +1,15 @@
+// var textinput = document.getElementById("otherallergy");
+
+// console.log(textinput.value)
+
+// if (textinput.checked != true){
+//     textinput.style.background = "var(--accent)";
+// }
+
+function colorChange() {
+
+    var textinput = document.getElementById("otherallergy");
+    textinput.style.background = 'var(--accent';
+    textinput.style.color = 'var(--white)';
+    
+};
